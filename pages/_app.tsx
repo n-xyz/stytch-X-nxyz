0b9212b719253c61629f17cf08d8e050202591da
Joshua Davis <joshua@neeva.co>
2022-07-18 15:56:26 -0700

    Fix build errors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "../styles/globals.css"
 import { StytchProvider, initStytch } from "@stytch/stytch-react"
 import type { AppProps } from "next/app"
+import Head from "next/head"
 import Script from "next/script"
 
 const stytch = initStytch(process.env.NEXT_PUBLIC_STYTCH_PUBLIC_TOKEN || "")
@@ -8,6 +9,9 @@ const stytch = initStytch(process.env.NEXT_PUBLIC_STYTCH_PUBLIC_TOKEN || "")
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <Script
         src="https://js.stytch.com/stytch.js"
         strategy="beforeInteractive"
