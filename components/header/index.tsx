3ad7c44d59c7e003e74b85b36bd4230953176245
Paul Lockett <Plockett.pl@gmail.com>
2022-07-21 17:12:13 -0700

    Address code comments

---
+++ components/header/index.tsx
@@ -0,0 +1,48 @@
+import { useStytchLazy } from "@stytch/stytch-react"
+import Image from "next/image"
+import Link from "next/link"
+
+import nxyzLogo from "../../public/nxyz-color.svg"
+import plusIcon from "../../public/plus-color.svg"
+import stytchLogo from "../../public/stytch-color.svg"
+import styles from "../../styles/Home.module.css"
+
+interface HeaderProps {
+  signOut: boolean
+}
+
+export default function Header({ signOut }: HeaderProps) {
+  const stytch = useStytchLazy()
+  const onSignOut = async () => {
+    await stytch.session.revoke()
+  }
+  return (
+    <header className={styles.header}>
+      <div className={styles.githubLink}>
+        <Link href="https://github.com/neevaco/stytch-X-nxyz">
+          <a target="_blank" rel="noopener noreferrer">
+            Github
+          </a>
+        </Link>
+      </div>
+      <div>
+        <Link href="https://stytch.com/">
+          <a target="_blank" rel="noopener noreferrer">
+            <Image src={stytchLogo} height={100} width={150} alt="stytch" />
+          </a>
+        </Link>
+        <Image src={plusIcon} height={100} width={50} alt="" />
+        <Link href="https://n.xyz/">
+          <a target="_blank" rel="noopener noreferrer">
+            <Image src={nxyzLogo} height={100} width={130} alt="nxyz" />
+          </a>
+        </Link>
+      </div>
+      {signOut ? (
+        <button type="button" className={styles.signOut} onClick={onSignOut}>
+          Sign Out
+        </button>
+      ) : null}
+    </header>
+  )
+}
--- pages/wallet/index.tsx
+++ pages/wallet/index.tsx
@@ -1,21 +1,16 @@
 import { useEffect, useState } from "react"
 
-import { useStytchUser, useStytchLazy } from "@stytch/stytch-react"
+import { useStytchUser } from "@stytch/stytch-react"
 import Head from "next/head"
-import Image from "next/image"
-import Link from "next/link"
 import { useRouter } from "next/router"
 
-import WalletView from "../../components/getWallet/nxyzWallet"
-import nxyzLogo from "../../public/nxyz-color.svg"
-import plusIcon from "../../public/plus-color.svg"
-import stytchLogo from "../../public/stytch-color.svg"
+import Header from "../../components/header"
+import WalletView from "../../components/Wallet"
 import styles from "../../styles/Home.module.css"
 
 export default function Wallet() {
   const router = useRouter()
   const user = useStytchUser()
-  const stytch = useStytchLazy()
   const [walletAddress, setWalletAddress] = useState("")
 
   useEffect(() => {
@@ -32,50 +27,13 @@ export default function Wallet() {
     }
   }, [user, router])
 
-  const signOut = async () => {
-    await stytch.session.revoke()
-  }
   return (
     <div className={styles.container}>
       <Head>
         <title>nxyz ETH Wallet Data</title>
       </Head>
       <main className={styles.main}>
-        <header className={styles.header}>
-          <div className={styles.githubLink}>
-            <Link href="https://github.com/neevaco/stytch-X-nxyz">
-              <a target="_blank" rel="noopener noreferrer">
-                Github
-              </a>
-            </Link>
-          </div>
-          <div>
-            <Link href="https://stytch.com/">
-              <a target="_blank" rel="noopener noreferrer">
-                <Image
-                  src={stytchLogo}
-                  height={100}
-                  width={150}
-                  alt="stytch logo"
-                />
-              </a>
-            </Link>
-            <Image src={plusIcon} height={100} width={50} alt="plus Icon" />
-            <Link href="https://n.xyz/">
-              <a target="_blank" rel="noopener noreferrer">
-                <Image
-                  src={nxyzLogo}
-                  height={100}
-                  width={130}
-                  alt="nxyz logo"
-                />
-              </a>
-            </Link>
-          </div>
-          <button type="button" className={styles.signOut} onClick={signOut}>
-            Sign Out
-          </button>
-        </header>
+        <Header signOut />
         {walletAddress === "" ? (
           <p className={styles.description}>
             Please sign out and use an Ethereum wallet
